f7243adcac777f1bd5d09d8acfaa3962d73a54e9
nakira974 <maxime.loukhal@gmail.com>
2023-05-29 01:26:49 +0200

    Add missing files for Xml ser

---
+++ Lkhsoft.Serialization/Lkhsoft.Serialization.Activities.Design/Designers/XmlDeserializationDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace Lkhsoft.Serialization.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for XmlDeserializationDesigner.xaml
+    /// </summary>
+    public partial class XmlDeserializationDesigner
+    {
+        public XmlDeserializationDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
